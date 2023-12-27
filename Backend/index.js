bf5223210c30cedb157d359464ce5f8d09d205eb
TanviSharma2104 <tanvisharma2703@gmail.com>
2023-12-27 23:11:51 +0530

    Backend server added

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect';
+
+dotenv.config();// allows to import environment variables from dotenv
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', async (req, res) => {
+    res.send("Hello from DALL-E!");
+});
+
+const startServer=async ()=>{
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(3000, ()=> console.log("Server has started on port http://localhost:3000"));
+    }
+    catch(error){
+        console.log(error);
+    }
+    
+}
+
+startServer();
